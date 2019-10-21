2482caf62d1d08dbdc8a913210614e8b5c21aa7f
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 19:19:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,18 @@
 require_relative "./user.rb"
 
 class Student < User 
-
+  @knowledge
+  
   def initialize
+    @knowledge = []
   end
+  
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end
+  
+  def knowledge
+    @knowledge
+  end
+  
 end
